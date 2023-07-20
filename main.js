211ad469a87c56c55c2e5497fabcdf03abcdd1ec
rudra-9197 <91685979+rudra-9197@users.noreply.github.com>
2023-07-20 13:45:19 +0530

    add: Loading button

--- main.js
+++ main.js
@@ -4,13 +4,16 @@ let result_user_stat = document.getElementById("result-user-stat");
 let result_comp_stat = document.getElementById("result-comp-stat");
 let result_final_stat = document.getElementById("result-final-stat");
 const choices = document.getElementsByClassName("choices");
- document.getElementsByClassName("lds-spinner");
+
 let usr_slctn = document.getElementById("usr_slctn");
 let comp_slctn = document.getElementById("comp_slctn");
-let result = document.getElementById("result");
+let result = document.getElementById("reslt");
 
 let tempUserScore = 0;
 let tempCOmpscore = 0;
+
+let usr_txt = "U have won this time!!!";
+let comp_txt = "Computer has won this time ..better luck next time";
 
 for (let choice of choices) {
   choice.addEventListener("click", updateScore);
@@ -21,21 +24,22 @@ function compChoice() {}
 function updateScore(e) {
   let userChoice = e.target.alt;
   usr_slctn.innerText = userChoice.toUpperCase();
-  let compChoice = computerchoice();
-  
 
-  let val = setTimeout(()=>{
-    resultupdate(userChoice, compChoice)
-  },2000);
-  
-  
+  load() ;
+
+  let compChoice = computerchoice();
+
+  let val = setTimeout(() => {
+    resultupdate(userChoice, compChoice);
+  }, 3000);
+
   if (tempUserScore == 3) {
     alert("U have won this Game");
-    location.reload()
+    location.reload();
   }
   if (tempCOmpscore == 3) {
     alert("Computer have won! Play again");
-    location.reload()
+    location.reload();
   }
 }
 
@@ -49,47 +53,57 @@ function computerchoice() {
   return compChoice;
 }
 
-function resultupdate(userval, compval) {
 
+function load() {
+    document.getElementById("loader").style.display = "block";
+    setTimeout(function(){
+      document.getElementById("loader").style.display = "none"; }, 2500);
+  }
+
+
+
+
+function resultupdate(userval, compval) {
   comp_slctn.innerText = compval.toUpperCase();
   if (tempUserScore <= 3 && tempCOmpscore <= 3) {
     if (userval == "rock") {
       if (userval == compval) {
-        result_final_stat.innerText = "ohh!.It's a Tie..Go On";
+        result.innerText = "ohh!.. It's a Tie.Go On";
+        
       } else if (compval == "scissors") {
         tempUserScore += 1;
         userScoreVal.innerText = tempUserScore;
-        result_final_stat.innerText = "U have won this time!!!";
+        result.innerText = "U have won this time!!!";
       } else if (compval == "paper") {
         tempCOmpscore += 1;
         compScoreVal.innerText = tempCOmpscore;
-        result_final_stat.innerText =
+        result.innerText =
           "Computer has won this time ..better luck next time";
       }
     } else if (userval == "paper") {
       if (userval == compval) {
-        result_final_stat.innerText = "ohh!.. It's a Tie.Go On";
+        result.innerText = "ohh!.. It's a Tie.Go On";
       } else if (compval == "rock") {
         tempUserScore += 1;
         userScoreVal.innerText = tempUserScore;
-        result_final_stat.innerText = "U have won this time!!!";
+        result.innerText = "U have won this time!!!";
       } else {
         tempCOmpscore += 1;
         compScoreVal.innerText = tempCOmpscore;
-        result_final_stat.innerText =
+        result.innerText =
           "Computer has won this time ..better luck next time";
       }
     } else if (userval == "scissors") {
       if (userval == compval) {
-        result_final_stat.innerText = "ohh!.. It's a Tie.Go On";
+        result.innerText = "ohh!.. It's a Tie.Go On";
       } else if (compval == "paper") {
         tempUserScore += 1;
         userScoreVal.innerText = tempUserScore;
-        result_final_stat.innerText = "U have won this time!!!";
+        result.innerText = "U have won this time!!!";
       } else {
         tempCOmpscore += 1;
         compScoreVal.innerText = tempCOmpscore;
-        result_final_stat.innerText =
+        result.innerText =
           "Computer has won this time ..better luck next time";
       }
     }
